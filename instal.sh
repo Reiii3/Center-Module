bc0cfa182672e351f4c5f68b02c0858ea5053836
Reiii3 <mohtoyib79@gmail.com>
2024-12-29 00:53:32 +0700

    update instal.sh

--- instal.sh
+++ instal.sh
@@ -36,6 +36,7 @@ echo
 sleep 1
 
 if [ ${runPackage} = com.mojang.minecraftpe ]; then
+  echo "                       Special Edition"
   echo "${wr}           █▄─▀█▀─▄█─▄▄▄─█▄─▄▄─█▄─▄▄─█${nwr}"
   echo "${wr}           ██─█▄█─██─███▀██─▄▄▄██─▄█▀█${nwr}"
   echo "${wr}           ▀▄▄▄▀▄▄▄▀▄▄▄▄▄▀▄▄▄▀▀▀▄▄▄▄▄▀${nwr}"
