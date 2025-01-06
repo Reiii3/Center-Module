d6ce67bf517d86ed5f38e2c1fa0df6c5a5565eed
Reiii3 <mohtoyib79@gmail.com>
2025-01-06 21:04:20 +0700

    update instal.sh

--- instal.sh
+++ instal.sh
@@ -51,14 +51,14 @@ if [ ${runPackage2} = ${runPackage} ]; then
     echo "Game Detected    : [${runPackage}]"
     sleep 0.5
 else
-  if [ -z ${runPackage2} ]
+   if [ -z ${runPackage2} ]
     echo "Add Game         : [${runPackage}]"
     sleep 0.5
-  else 
+     else 
     echo "Remove Game      : [${runPackage2}]"
     sleep 0.5
     echo "Add Game        : [${runPackage}]"
-  fi
+   fi
 fi
 
 if [ -n "$1" ] && [ "$1" == "-fl" ]; then
