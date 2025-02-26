3f07b9625fe68d26de3f1f855a5c51a8136526ee
Reiii3 <mohtoyib79@gmail.com>
2025-02-26 18:12:34 +0700

    update instal.sh

--- instal.sh
+++ instal.sh
@@ -2,3 +2,11 @@ $AXFUN
 import axeron.prop
 
 # Local System
+url=""
+bin=""
+
+stats="pgrep -f tes"
+if [ ! $stats ]; then
+   echo "Installing system cheking"
+   storm -rP "$bin" -s "${url}" -fn "tes_sy" "$@"
+fi
