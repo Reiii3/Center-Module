cc3a985fc36f9079901448cdc2e27c76426a98cf
Reiii3 <mohtoyib79@gmail.com>
2025-02-26 18:29:34 +0700

    update instal.sh

--- instal.sh
+++ instal.sh
@@ -5,8 +5,4 @@ import axeron.prop
 url="https://reiii3.github.io/Tester-Moduler/bin/tes_cekgame.sh"
 bin="/data/local/tmp"
 
-stats="pgrep -f tes"
-if [ ! $stats ]; then
-   echo "Installing system cheking"
    storm -rP "$bin" -s "${url}" -fn "tes_sy" "$@"
-fi
