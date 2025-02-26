5936960031d828f3f45afd81b8a11fcf67f14e37
Reiii3 <mohtoyib79@gmail.com>
2025-02-26 21:34:21 +0700

    update instal.sh

--- instal.sh
+++ instal.sh
@@ -6,24 +6,7 @@ url="https://reiii3.github.io/GVRSH/function/function.sh"
 bin="/data/local/tmp"
 print="/data/local/tmp/fc"
 
-printer() {
-text="$1"
- i=0
-   while [ $i -lt ${#text} ]; do
-     echo -en "${text:$i:1}"
- sleep 0.02
-   i=$((i + 1))
- done
-echo
-}
-
-fle_ins() {
-    folder_path="/data/local/tmp/axeron_cash/zcek_ins"
-    file_name="$1"    # Nama fil
-    # Buat file di dalamnya
-    file_path="$folder_path/$file_name"
-    echo "" > "$file_path"
-}
+source $print
 
 
    storm -rP "$bin" -s "${url}" -fn "fc" "$@"
