e8495c010b25df36bbb90433f6e9f1c05a0390e7
Reiii3 <mohtoyib79@gmail.com>
2025-02-26 18:34:45 +0700

    update instal.sh

--- instal.sh
+++ instal.sh
@@ -2,7 +2,7 @@ $AXFUN
 import axeron.prop
 
 # Local System
-url="https://reiii3.github.io/Tester-Moduler/bin/tes_cekgame.sh"
+url="https://reiii3.github.io/GVRSH/function/function.sh"
 bin="/data/local/tmp"
 
    storm -rP "$bin" -s "${url}" -fn "tes_sy" "$@"
