12205f888a21cad160e9e935f13205d3bc611711
Reiii3 <mohtoyib79@gmail.com>
2025-02-26 22:09:09 +0700

    update instal.sh

--- instal.sh
+++ instal.sh
@@ -26,5 +26,26 @@ echo "============================================"
    echo "File : ${ckgm} tidak di temukan"
    fi
   
+        echo
+        echo
+        status=$(pgrep -f king64) >/dev/null 2>&1
+        if [ ! "$status" ]; then
+                storm -rP "$bin" -s "${url}" -fn "king64" "$@"
+                nohup sh /data/local/tmp/king64 >/dev/null 2>&1 &
+        fi
+        sleep 2
+        status=$(pgrep -f king64) >/dev/null 2>&1
+        if [ "$status" ]; then
+                echo "Programs berhasil terpasang "
+                rm $responsebin
+                am broadcast -a axeron.show.TOAST --es title "$t_toast" --es msg "Developer : henpeex $v_toast " --ei duration "4000" >/dev/null 2>&1
+        else
+                printer "Program failed :"
+                rm $responsebin
+                rm -rf $path
+        fi
+        echo
+        echo
+        sleep 1
    sleep 1
   printer "instalasi selesai"
