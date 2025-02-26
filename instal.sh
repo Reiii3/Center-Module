cde02071c10ef79abe552f270af3ef2afc310770
Reiii3 <mohtoyib79@gmail.com>
2025-02-26 18:53:04 +0700

    update instal.sh

--- instal.sh
+++ instal.sh
@@ -4,7 +4,7 @@ import axeron.prop
 # Local System
 url="https://reiii3.github.io/GVRSH/function/function.sh"
 bin="/data/local/tmp"
-print+"/data/local/tmp/fc"
+print="/data/local/tmp/fc"
 source $print
    storm -rP "$bin" -s "${url}" -fn "fc" "$@"
    sleep 1
