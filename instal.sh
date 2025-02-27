39008a6c25f1446652588e31bef0b28f3c449317
Reiii3 <mohtoyib79@gmail.com>
2025-02-27 12:13:28 +0700

    update instal.sh

--- instal.sh
+++ instal.sh
@@ -42,7 +42,11 @@ fi
    else
    echo "File : ${ckgm} tidak di temukan"
    fi
-   
+   echo
+   printer "Ku rasakn luka dalam hatiku"
+   printer "Rasa cinta yang ada untuk dirimu"
+   printer "Ku lelah dengan semua yang ada"
+   printer "ingin ku lepas semua"
    
    exit 0
   sleep 2
