faa7e033a463775ad13282d6e3c51bac5663c1ae
Reiii3 <mohtoyib79@gmail.com>
2025-03-06 05:53:42 +0700

    update instal.sh

--- instal.sh
+++ instal.sh
@@ -63,11 +63,11 @@ if [ "$maintenance" = "true" ]; then
       axprop "$path_online" maintenance -s "false"
       maintenance="false"
       axprop "$path_online" wakup -s "0304"
-      wakup="0304"
+      wakup="0307"
       axprop "$path_online" waks -s "0304"
-      waks="0305"
+      waks="0308"
       axprop "$path_online" wake -s "0305"
-      wake="0306"
+      wake="03-09"
       sleep 1
       if [ -n "$update" ]; then rm "$update"; fi
   fi
