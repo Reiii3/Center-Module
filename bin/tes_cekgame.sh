5d2a71a617d409b687c06ce069cdf0639679f98a
Reiii3 <mohtoyib79@gmail.com>
2025-03-09 05:42:53 +0700

    update bin/tes_cekgame.sh

--- bin/tes_cekgame.sh
+++ bin/tes_cekgame.sh
@@ -8,36 +8,41 @@ gamerun=""
 notif_run=""
 
 ai_start() {
- setptop debug.hwui.renderer skiavk 
+    setprop debug.hwui.renderer skiavk 
 }
+
 . $engine
+
 check_game() {
-detected_apps=$(dumpsys window | grep -E 'mCurrentFocus|mFocusedApp' | awk -F'[ /}]' '{print $5}' | tail -n 1)
-render_detected=$(getprop )
+    detected_apps=$(dumpsys window | grep -E 'mCurrentFocus|mFocusedApp' | awk -F'[ /}]' '{print $5}' | tail -n 1)
+    render_detected=$(getprop debug.hwui.renderer)
+
     if [ -n "$detected_apps" ]; then
         if [ "$gamerun" != "running" ]; then
             if [ "$runPackage" = "$detected_apps" ]; then
                 echo "Game sedang dimainkan: $detected_apps"
-                if [ $notif_run != "run" ]; then
-                  run='cmd notification post -S bigtext -t "Game Detected" "game_log" "Game sedang dimainkan: '"$detected_apps"'"'
-                  eval "$run"
-                  notif_tun="run"
+                if [ "$notif_run" != "run" ]; then
+                    run='cmd notification post -S bigtext -t "Game Detected" "game_log" "Game sedang dimainkan: '"$detected_apps"'"'
+                    eval "$run"
+                    notif_run="run"
                 fi
             fi
             sleep 0.5
             gamerun="running"
+            notif_run=""  # Reset supaya bisa berubah lagi
         fi
         IDLE_TIME=5 
     else
         if [ "$gamerun" != "stopped" ]; then
             echo "Tidak ada game yang berjalan"
-              if [ $notif_run != "stop" ]; then
+            if [ "$notif_run" != "stop" ]; then
                 stop='cmd notification post -S bigtext -t "Game closed" "game_log" "Tidak ada game yang berjalan"'
                 eval "$stop"
                 notif_run="stop"
-              fi
+            fi
             sleep 0.5
             gamerun="stopped"
+            notif_run=""  # Reset supaya bisa berubah lagi
         fi
         IDLE_TIME=7.5 
     fi
