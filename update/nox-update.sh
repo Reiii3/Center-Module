5c49b143ee5df396ae171fad7020fe3d3c13b7f3
Reiii3 <mohtoyib79@gmail.com>
2025-05-03 12:37:46 +0700

    update update/nox-update.sh

--- update/nox-update.sh
+++ update/nox-update.sh
@@ -1,5 +1,5 @@
 {
   noxUpdate=true
   info="versi 1.3.0-beta akan segera siap, dengan kode engine NXV130B masih dalam proses...
-  kemungkinan update akan selesai pafa 20.00 WIB"
+  kemungkinan update akan selesai pada 20.00 WIB"
 }
